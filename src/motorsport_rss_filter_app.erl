1dcbc412642f61aa5b135f7a2ea890d06a371683
Steve Roques <steve.roques@gmail.com>
2026-05-28 12:43:21 +0200

    feat: migrate to Phase 3 (em_pop gossip + Cowboy HTTP)
    Replace em_filter:start_agent/3 WebSocket pattern with:
    - OTP supervisor (motorsport_rss_filter_sup)
    - gen_server handler (motorsport_rss_filter_server) with persistent Memory state
    - em_pop_sup:start_node/2 for gossip peer discovery
    - Cowboy HTTP via em_filter_http for direct queries
    
    Update em_filter dep: 1.2.4 -> 1.2.5
    Add config/sys.config with ports and gossip seeds

--- src/motorsport_rss_filter_app.erl
+++ src/motorsport_rss_filter_app.erl
@@ -12,26 +12,70 @@
 -behaviour(application).
 
 -export([start/2, stop/1]).
+-export([handle/2, base_capabilities/0]).
 
 %%====================================================================
-%% Application callbacks
+%% Application lifecycle
 %%====================================================================
 
-start(_StartType, _StartArgs) ->
+start(_Type, _Args) ->
     copy_config(),
-    application:ensure_all_started(rss_filter),
-    em_filter:start_agent(motorsport_rss_filter, rss_filter_app, #{
-        capabilities => rss_filter_app:base_capabilities()
-                        ++ [<<"motorsport">>, <<"f1">>, <<"rally">>, <<"wrc">>]
-    }),
-    {ok, self()}.
+    case motorsport_rss_filter_sup:start_link() of
+        {ok, Pid} ->
+            ok = start_pop_and_http(),
+            {ok, Pid};
+        Error ->
+            Error
+    end.
 
 stop(_State) ->
+    catch cowboy:stop_listener(motorsport_rss_filter_query_listener),
+    catch em_pop_sup:stop_node(motorsport_rss_filter),
     ok.
+
+%%====================================================================
+%% Capabilities and handler
+%%====================================================================
+
+-spec base_capabilities() -> [binary()].
+base_capabilities() ->
+    rss_filter_app:base_capabilities() ++ [<<"motorsport">>, <<"f1">>, <<"rally">>, <<"wrc">>].
+
+-spec handle(binary(), map()) -> {list(), map()}.
+handle(Body, Memory) ->
+    rss_filter_app:handle(Body, Memory).
 
 %%====================================================================
 %% Internal
 %%====================================================================
+
+start_pop_and_http() ->
+    PopPort   = application:get_env(motorsport_rss_filter, pop_port,   9468),
+    QueryPort = application:get_env(motorsport_rss_filter, query_port, 9469),
+    Seeds     = application:get_env(motorsport_rss_filter, pop_seeds,  []),
+    Vec = em_filter_vec:from_capabilities(base_capabilities()),
+    catch em_pop_sup:stop_node(motorsport_rss_filter),
+    catch cowboy:stop_listener(motorsport_rss_filter_query_listener),
+    {ok, PopPid} = em_pop_sup:start_node(motorsport_rss_filter, #{
+        port            => PopPort,
+        query_port      => QueryPort,
+        vector          => Vec,
+        max_peers       => 100,
+        gossip_interval => 5_000
+    }),
+    lists:foreach(
+        fun({H, P}) -> catch em_pop_node:add_peer(PopPid, H, P) end,
+        Seeds),
+    Dispatch = cowboy_router:compile([
+        {'_', [{"/agent/query", em_filter_http,
+                #{server => motorsport_rss_filter_server}}]}
+    ]),
+    {ok, _} = cowboy:start_clear(motorsport_rss_filter_query_listener,
+                                  [{port, QueryPort}],
+                                  #{env => #{dispatch => Dispatch}}),
+    logger:notice("[motorsport_rss_filter] gossip port ~w  query port ~w",
+                  [PopPort, QueryPort]),
+    ok.
 
 copy_config() ->
     case code:priv_dir(motorsport_rss_filter) of
---
+++ src/motorsport_rss_filter_sup.erl
@@ -0,0 +1,23 @@
+%%%-------------------------------------------------------------------
+%%% @doc motorsport_rss_filter supervisor.
+%%%
+%%% Supervises the motorsport_rss_filter_server gen_server.
+%%% @end
+%%%-------------------------------------------------------------------
+-module(motorsport_rss_filter_sup).
+-behaviour(supervisor).
+
+-export([start_link/0, init/1]).
+
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+init([]) ->
+    ServerSpec = #{
+        id      => motorsport_rss_filter_server,
+        start   => {motorsport_rss_filter_server, start_link, []},
+        restart => permanent,
+        type    => worker
+    },
+    {ok, {#{strategy => one_for_one, intensity => 3, period => 10},
+          [ServerSpec]}}.
